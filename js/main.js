0391cd1983f4e91b34ebc0d130e5de2c6aa5e8a5
Aleshka <xyu@gmail.com>
2022-03-31 20:37:09 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+"use strict";
+let number = 5;
+const leftBorderWidth = 15;
+ var name = 'Ivan';
+
+// alert('dsadsadasdasd;')
+
+ confirm("Ты здесь пидр?");
+
+ const owo = prompt("Ты ублюдок?");
+
+ const category = 'cat';
+ console.log(`https://someurl.com/${category}/5`);
