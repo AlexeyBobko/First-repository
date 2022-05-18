b2ae9d7f7df8b4e09d0455c550d3e2149c990450
Alex <lexabobko@gmail.com>
2022-05-18 20:50:32 +0300

    Triangle and marks

--- js/main.js
+++ js/main.js
@@ -92,3 +92,27 @@ for (let i = 1; i < 10; i++) {
     }
     console.log(i);
 }
+// Задача на построение треугольника
+let result = '';
+const length = 7;
+
+for(let i = 1; i < length; i++){
+
+    for( let j = 0; j < i; j++){
+        result += '*';
+    }
+    result += '\n';
+}
+console.log(result);
+
+// Метки( в данном случае меткой является first)
+first: for(let i = 0; i < 3; i++){
+    console.log(`First level: ${i}`);
+    for(let j = 0; j < 3; j++){
+        console.log(`Second level: ${j}`);
+        for(let k = 0; k < 3; k++){
+            if (k===2) continue first;
+            console.log(`Third level: ${k}`);
+        }
+    }
+}
